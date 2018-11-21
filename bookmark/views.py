d77a85dbb2ad0049c8592768ab46f58ec13fd52d
Mux <dnfl7080@daum.net>
2018-11-21 16:47:17 +0900

    Test Page

---
+++ bookmark/views.py
@@ -0,0 +1,8 @@
+from bookmark.models import Bookmark
+from django.views.generic import ListView, DetailView
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
