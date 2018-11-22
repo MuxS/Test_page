3f3df74360952d52fb516e556418480ee350e727
Mux <dnfl7080@daum.net>
2018-11-22 17:26:16 +0900

    search update

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthA
 from blog.models import Post
 from tagging.models import Tag, TaggedItem
 from tagging.views import TaggedObjectList
+from django.views.generic.edit import FormView
+from blog.forms import PostSearchForm
+from django.db.models import Q
+from django.shortcuts import render
 
 # ListView
 class PostLV(ListView):
@@ -44,3 +48,18 @@ class TagTV(TemplateView):
 class PostTOL(TaggedObjectList):
     model = Post
     template_name = 'tagging/tagging_post_list.html'
+
+# FormView
+class SearchFormView(FormView):
+    form_class = PostSearchForm
+    template_name = 'blog/post_search.html'
+
+    def form_valid(self, form):
+        schWord = '%s' % self.request.POST['search_word']
+        post_list = Post.objects.filter(Q(title__icontains=schWord) | Q(description__icontains=schWord) |
+                                        Q(content__icontains=schWord)).distinct()
+        context = {}
+        context['form'] = form
+        context['search_term'] = schWord
+        context['object_list'] = post_list
+        return render(self.request, self.template_name, context)
